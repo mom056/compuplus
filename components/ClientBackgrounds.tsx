3ce1d995d4156f5cd6f39709d91aab9ab51f3385
Mohamed Abdelmonem <mom056@users.noreply.github.com>
2026-01-17 21:56:30 +0200

    revert: remove deferred loading of backgrounds to fix UX pop-in

--- components/ClientBackgrounds.tsx
+++ components/ClientBackgrounds.tsx
@@ -12,19 +12,6 @@ const AIChatbot = dynamic(() => import('@/components/AIChatbot'), {
 });
 
 export default function ClientBackgrounds() {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        // Defer loading of heavy background elements to prioritize LCP content
-        const timer = setTimeout(() => {
-            setIsMounted(true);
-        }, 2000);
-
-        return () => clearTimeout(timer);
-    }, []);
-
-    if (!isMounted) return null;
-
     return (
         <>
             <ParticleBackground />
